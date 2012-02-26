7eed0c692f4d4fc1a4bdf21b96ce270e43432d9a
Dan Loewenherz <dloewenherz@gmail.com>
2012-02-26 13:51:46 -0500

    bump version to 1.0.0

--- objectifier/__init__.py
+++ objectifier/__init__.py
@@ -12,7 +12,7 @@ objectifier
 from .objectifier import Objectifier
 
 __all__ = ['Objectifier']
-__version__ = 
+__version__ = 1.0.0
 __author__ = "Dan Loewenherz"
 __copyright__ = "Copyright 2012, Elm City Labs, LLC"
 __maintainer__ = "Dan Loewenherz"
