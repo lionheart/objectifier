4d37aaa16479ffbe2a94dac64eed44c266474581
Dan Loewenherz <dloewenherz@gmail.com>
2012-02-26 12:32:43 -0500

    add docstrings and LICENSE

--- objectifier/__init__.py
+++ objectifier/__init__.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 
 from objectifier import Objectifier
 
 __all__ = ['Objectifier']
 __version__ = 1.0.0
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2012, Elm City Labs, LLC"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@elmcitylabs.com"
+__license__ = "Apache 2.0"
 
