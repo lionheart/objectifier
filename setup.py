bd830f33c884f831da74724db3663db765586249
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-14 14:40:37 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
+import os
 from objectifier import metadata
 
 try:
@@ -8,15 +9,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='objectifier',
     version=metadata.__version__,
     url='https://github.com/elmcitylabs/objectifier',
     license=metadata.__license__,
+    long_description=read('README.rst'),
     description='Turns dictionaries into Python objects.',
     author=metadata.__author__,
     author_email=metadata.__email__,
     packages=['objectifier'],
-    package_data={'': ['LICENSE', 'README']},
+    package_data={'': ['LICENSE']},
 )
 
