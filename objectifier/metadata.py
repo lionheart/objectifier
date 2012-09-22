a982e94b88a81e34f28a80f53f85615815f456e8
Dan Loewenherz <dloewenherz@gmail.com>
2012-09-22 12:08:10 -0700

    update metadata

--- objectifier/metadata.py
+++ objectifier/metadata.py
@@ -1,7 +1,7 @@
-__version__ = "1.2.4"
+__version__ = "1.3.0"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2012, Elm City Labs LLC"
+__copyright__ = "Copyright 2012, Dan Loewenherz"
 __maintainer__ = "Dan Loewenherz"
-__email__ = "dan@elmcitylabs.com"
+__email__ = "dan@dlo.me"
 __license__ = "Apache 2.0"
 
